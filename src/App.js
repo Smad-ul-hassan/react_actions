de4dbbadb2010386dbb722090945b9313291839b
Smad <93592306+Smad-ul-hassan@users.noreply.github.com>
2023-03-17 16:34:36 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,18 +8,19 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         {/* {/* <p> */}
         Edit <code>src/App.js</code> and save to reload.
-        {/* </p> */} */}
-        {/* <></>
+
+        <p>
+          {/* <></>
         <div> */}
-        <></>
-        <></>     <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <></>
+          <></>     <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
       </header>
     </div >
   );
