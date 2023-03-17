8c45cbe35a320222f116f4737bd30446f50bb447
Smad <93592306+Smad-ul-hassan@users.noreply.github.com>
2023-03-17 14:49:10 +0500

    feature: initial commit

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ function App() {
         {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p> */}
+        {/* <></>
+        <div> */}
         <></>
-        <div>
-          <></>
-          <></>     <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <></>     <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
